9fd155c7eb937d7514c550c1f23b4763b8013c38
Nishant <nishantkarekar98@gmail.com>
2024-12-22 18:41:22 +0530

    "add changes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import Background from "./components/Background";
 import ForGround from "./components/ForGround";
+
 function App() {
   return (
     <div className="relative bg-zinc-800 h-screen w-full">
